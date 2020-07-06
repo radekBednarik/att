e8576d1f592bfc119d8cad368ee429d8c5c39c10
dzcxs6c <radek.bednarik@tesena.com>
2020-07-06 19:11:57 +0200

    refactoring

--- tests/test_api_calls_ok.py
+++ tests/test_api_calls_ok.py
@@ -12,35 +12,29 @@ from tests.auth import API_KEY
     params=["/czso.cz/lide-domy-byty", "/cnb.cz/smenarny"],
     ids=["/czso.cz/lide-domy-byty", "/cnb.cz/smenarny"],
 )
-def valid_single_resource(request):
-    api = API(API_KEY)
-    return api.query(request.param)
-
-
-@pytest.fixture(
-    params=["/czso.cz/lide-domy-byty", "/cnb.cz/smenarny"],
-    ids=["/czso.cz/lide-domy-byty", "/cnb.cz/smenarny"],
-)
-def valid_whole_resource(request):
+def valid_resource(request):
     return request.param
 
 
 class TestWorkingApiCalls:
-    def test_query(self, valid_single_resource):
-        assert isinstance(valid_single_resource, ApiResponse) is True
-
-    def test_get_all_no_params(self, valid_whole_resource):
+    def test_query(self, valid_resource):
         api = API(API_KEY)
-        data = api.get_all(valid_whole_resource, sleep=0.1)
+        r = api.query(valid_resource)
+
+        assert isinstance(r, ApiResponse) is True
+
+    def test_get_all_no_params(self, valid_resource):
+        api = API(API_KEY)
+        data = api.get_all(valid_resource, sleep=0.1)
 
         assert isinstance(data, list) is True
         assert len(data) > 0
         assert all([isinstance(item, dict) for item in data]) is True
 
-    def test_get_all_params_set_01(self, valid_whole_resource):
+    def test_get_all_params_set_01(self, valid_resource):
         api = API(API_KEY)
         data = api.get_all(
-            valid_whole_resource, limit=10, skip=30, order='"id ASC"', sleep=0.1,
+            valid_resource, limit=10, skip=30, order='"id ASC"', sleep=0.1,
         )
 
         assert isinstance(data, list) is True
