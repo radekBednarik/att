ca52d9ca29f53760b0ec072cbe6c5fd54d25de66
dzcxs6c <radek.bednarik@tesena.com>
2020-07-09 17:56:21 +0200

    bump up version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Radek 'bednaJedna' Bednarik"
 author = "Radek 'bednaJedna' Bednarik"
 
 # The full version, including alpha/beta/rc tags
-release = "0.6.0"
+release = "0.6.2"
 
 
 # -- General configuration ---------------------------------------------------
