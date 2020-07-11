4e0f6e3bb25ade79810306982cb66bb11861bf7a
bednaJedna <bednarik.radek@gmail.com>
2020-07-11 07:37:38 +0200

    added links to source code in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ release = "1.0.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.viewcode"]
 
 master_doc = "index"
 
@@ -63,6 +63,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinxdoc"
+html_show_sourcelink = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
